5ad3e1ce127cc2a8106f644f10226b46d95e0923
chao_zhang <chao_zhang@eventown.com>
2017-07-18 16:44:32 +0800

    操作redis

--- test/commondRedis.py
+++ test/commondRedis.py
@@ -3,3 +3,18 @@
 import redis
 import configparser
 
+Config = configparser.ConfigParser()
+Config.read("../config/redis.ini")
+print(Config['redis']['host'])
+
+redisHost = Config.get("redis", "host")
+redisPort = Config.get("redis", "port")
+redisDb = Config.get("redis", "db")
+redis = redis.Redis(redisHost, redisPort, redisDb)
+
+print(redis)
+
+redis.set("erbi", "fengfeng")
+redis.set("sanpao", "guoguo")
+name = redis.get("erbi")
+print(name)
