678f6415a86bca2edf5afcbe83e60a1b0a9e1ef6
chao_zhang <chao_zhang@eventown.com>
2017-07-31 18:41:35 +0800

    操作redis

--- blog/usercenter/views.py
+++ blog/usercenter/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    print(u"我爱北京天安门")
+    return HttpResponse()
---
+++ test/a.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+def te():
+    i = 1
+    yield i
+
+
+r = te()
+print(1)
